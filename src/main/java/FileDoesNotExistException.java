24f76463a12436920130423096c3893cd892070f
Nandita Hazra <hazranandita2022@gmail.com>
2022-11-01 12:10:02 +0530

    Modified Java program: FileDoesNotExistException.java to remove 1 warning in the code.

--- src/main/java/FileDoesNotExistException.java
+++ src/main/java/FileDoesNotExistException.java
@@ -6,28 +6,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 // Main class.
 public class FileDoesNotExistException
 {   // Start of main class.
 
     // Starting point of execution.
-    public static void main(String args[])
+    public static void main(String[] args)
     {   // Start of execution thread.
 
         // try-catch block for exception handling.
-        try {
+        try
+        {   // Start of try block.
 
             // Following file does not exist.
             File file = new File("E://file.txt");
-            FileReader objFileReader = new FileReader(file);
-        }
-        catch (FileNotFoundException e) {
 
-            System.err.println("Caught FileNotFoundException: "
-                    + e.getMessage());
-
-        }
+            // Java in-built class to read file.
+            FileReader fr = new FileReader(file);
+            System.out.println("Reading char by char : \n");
+            int i;
+            while ((i = fr.read()) != -1) {
+                System.out.print((char) i);
+            }
+        }   // End of try block.
+        catch (FileNotFoundException e)
+        {   // Start of catch block.
+            System.err.println("In the Catch block for file not found exception.");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.err.println("In the Catch block for input/output exception.");
+            throw new RuntimeException(e);
+        }   // End of catch block.
 
     }   // End of execution thread.
 
